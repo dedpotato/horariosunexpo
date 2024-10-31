67bd8b3fcaa8660dd3c6e3367486ded75a18e166
dedpotato <77311120+dedpotato@users.noreply.github.com>
2024-10-31 09:56:43 -0400

    ya casi

--- function.js
+++ function.js
@@ -1,10 +1,9 @@
-
 function lafuncion(matriz) {
 
 var visualMatriz1 = '<ul>'
-matriz.forEach(function(matriz) {
-  visualMatriz1 += '<li>'+ matriz + '</li>';
-}); 
+for (a of matriz){
+    visualMatriz1 += '<li>'+ a + '</li>';
+}
 visualMatriz1 += '</ul>';
 
 let numeroDeMaterias =document.getElementById("numeromaterias").innerHTML
@@ -20,17 +19,23 @@ let tamano=materiasElegidas.length;
 for(let i = 0; i<materiasElegidas.length;i++){
     let a= [];
     for(let j = 0; j<materiasElegidas.length;j++){
-        //window.alert(materiasElegidas[i]+' '+materiasElegidas[j]+' '+matriz[materiasElegidas[i]][materiasElegidas[j]])
+        //console.log('i',i,'j',j,'materiasElegidas',materiasElegidas,matriz[30],materiasElegidas[i])
         a.push(matriz[materiasElegidas[i]][materiasElegidas[j]]);
     }
     matrizPequena.push(a);
 }
-
+let tabla ='<table>';
+    for (let b of materiasElegidas){  
+        c="zafo"+b
+        tabla+='<tr>'+document.getElementById(c).innerHTML+'</tr>';
+        //console.log(document.getElementById(c).innerHTML);
+    }
+    tabla+='</table>'
 var visualMatriz2 = '<ul>'
 matrizPequena.forEach(function(matrizPequena) {
   visualMatriz2+= '<li>'+ matrizPequena + '</li>';
 }); 
-visualMatriz2 += '</ul>';
+visualMatriz2 += '</ul>'+tabla;
 
 //codigo en desarrollo
 let listaNueva = materiasElegidas.map(i => [i]);
@@ -80,11 +85,19 @@ for (let i of listaAgregados) {
 
 //codigo en desarrollo
 
-var visualMatriz3 = '<ul>'
-listaTotal.forEach(function(a) {
-  visualMatriz3 += '<li>'+ a + '</li>';
-}); 
-visualMatriz3 += '</ul>';
+var visualMatriz3 = '<div>'
+for (let a of listaTotal) {
+    let tabla ='<table>';
+    for (let b of a){  
+        c="zafo"+b
+        tabla+='<tr>'+document.getElementById(c).innerHTML+'</tr>';
+        //console.log(document.getElementById(c).innerHTML);
+    }
+    tabla+='</table>'
+    visualMatriz3 +=  a +tabla+ "</br>";
+    
+}; 
+visualMatriz3 += '</div>';
 
 document.getElementById("result").innerHTML = '</br>'+
 visualMatriz1+'</br>'+materiasElegidas+'</br>'+visualMatriz2+'</br>'+visualMatriz3;
